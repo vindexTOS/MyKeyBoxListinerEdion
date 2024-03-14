1668ba938da89d2ac50903254a90b55d1f6d71b8
waska14 <waska@chaduneli.com>
2024-03-14 18:30:42 +0400

    Generate unique code and pass to proxy

--- src/controllers/base.js
+++ src/controllers/base.js
@@ -1,12 +1,15 @@
 module.exports = class Controller {
+    unique_code = '';
+
     constructor(locker, httpProxy) {
+        this.readOrCreateUniqueCode()
         this.locker = locker
         this.httpProxy = httpProxy.createProxyServer({
             target: 'https://mykeybox.com',
             changeOrigin: true,
             selfHandleResponse: true,
         })
-        this.httpProxy.on('proxyRes', function(proxyRes, req, res) {
+        this.httpProxy.on('proxyRes', function (proxyRes, req, res) {
             proxyRes.headers['access-control-allow-origin'] = '*';
             res.writeHead(proxyRes.statusCode, proxyRes.headers);
             proxyRes.pipe(res);
@@ -16,16 +19,44 @@ module.exports = class Controller {
             console.error('Proxy error', err);
             if (err.code === 'ETIMEDOUT') {
                 // Handle timeout error gracefully
-                res.writeHead(504, { 'Content-Type': 'text/plain' });
+                res.writeHead(504, {'Content-Type': 'text/plain'});
                 res.end('Gateway Timeout');
             } else {
                 // Handle other errors
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.writeHead(500, {'Content-Type': 'text/plain'});
                 res.end('Internal Server Error');
             }
         });
 
         this.httpProxy.timeout = 10000
+    }
+
+    readOrCreateUniqueCode() {
+        const fs = require('fs');
+        const filename = 'device.uniquecode.txt';
+
+        if (!fs.existsSync(filename)) {
+            const getRandomNumber = (min, max) => {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+
+            const generateRandomString = () => {
+                const date = new Date()
+                const day = String(date.getDay()).padStart(2, '0')
+                const month = String(date.getMonth()).padStart(2, '0')
+                const year = String(date.getFullYear()).substring(2)
+                const hour = String(date.getHours()).padStart(2, '0')
+                const minute = String(date.getMinutes()).padStart(2, '0')
+                const second = String(date.getSeconds()).padStart(2, '0')
+
+                const random = getRandomNumber(1000, 9999);
+                return `${random}-${hour}${month}-${day}${year}-${second}${minute}`;
+            }
+
+            fs.writeFileSync(filename, generateRandomString());
+        }
+
+        this.unique_code = fs.readFileSync(filename, 'utf8')
     }
 
     check(request, response) {
@@ -51,7 +82,8 @@ module.exports = class Controller {
     proxyToThirdPartyApi(url, request, response) {
         try {
             request.headers['ApiKey'] = 'z7#D4k9@A9'
-            request.url = 'Umbraco/Api/MyKeyBoxOrder/' + url
+            const separator = url.includes('?') ? '&' : '?'
+            request.url = 'Umbraco/Api/MyKeyBoxOrder/' + url + separator + 'uniquecode=' + this.unique_code
             this.httpProxy.web(request, response)
         } catch (err) {
             this.response(response, {
